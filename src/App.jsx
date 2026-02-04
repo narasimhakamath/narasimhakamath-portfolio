a96b544cb569577f077a078cc3b5b8e179b2feae
Narasimha Kamath <narasimha@datanimbus.com>
2026-02-04 21:02:41 +0530

    - Fixed the routing issue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { lazy, Suspense } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Stats from './components/Stats';
